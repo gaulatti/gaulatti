13ad69b03c25cefd04033c4c68fec8371f5ffcf9
Javier Godoy <javier@oxozon.io>
2025-04-11 18:42:13 -0400

    landed: Typos / Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-// @ts-check
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://gaulatti.com',
   integrations: [
     tailwind({
       configFile: './tailwind.config.js',
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
   compressHTML: true,
