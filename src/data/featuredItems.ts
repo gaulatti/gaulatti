b54006213d15cadbdd17ab65fc73f4854f2233f9
Javier Godoy Núñez <javier@oxozon.io>
2025-04-19 15:16:15 -0400

    landed: Sprint Names article

--- src/data/featuredItems.ts
+++ src/data/featuredItems.ts
@@ -13,9 +13,16 @@ interface HeroItem {
 export const featuredItems: HeroItem[] = [
   {
     type: 'post',
+    title: 'Alphabetical Acceleration: From Atacama to Zurich, One Sprint at a Time.',
+    description: 'How this simple alphabetic rule turns routine cycles into a motivating narrative and breaks the monotony of sprints.',
+    image: '/hero/sprints.avif',
+    link: 'https://blog.gaulatti.com/alphabetical-acceleration-from-atacama-to-zurich-one-sprint-at-a-time-8f1f22e96fca',
+    tags: ['Project Management', 'Scrum', 'Sprints'],
+  },
+  {
+    type: 'post',
     title: 'From Problem to Product: How Nazca turned into Live Streaming for Real-Time Earthquake Monitoring.',
-    description:
-      "Typically, technological products arise from an existing need. Ideas don't appear spontaneously; they don't emerge from nothing.",
+    description: "Typically, technological products arise from an existing need. Ideas don't appear spontaneously; they don't emerge from nothing.",
     image: '/hero/nazca.avif',
     link: 'https://blog.gaulatti.com/from-problem-to-product-how-nazca-turned-live-streaming-into-real-time-earthquake-monitoring-722428121737',
     tags: ['Streaming', 'Innovation', 'Monitoring', 'Automation'],
@@ -28,15 +35,6 @@ export const featuredItems: HeroItem[] = [
     image: '/hero/points.avif',
     link: 'https://medium.com/http418/nobody-can-agree-on-how-to-estimate-story-points-so-heres-my-approach-e2f499324103',
     tags: ['Agile', 'Planning', 'Estimation'],
-  },
-  {
-    type: 'project',
-    title: 'Autostrada',
-    description:
-      'Frontend observability, reimagined. Track performance, SEO, accessibility, and even aesthetics in real time — all from one modular, extensible platform.',
-    image: '/hero/autostrada.avif',
-    link: '/portfolio',
-    tags: ['Observability', 'Lighthouse', 'Performance'],
   },
   {
     type: 'post',
